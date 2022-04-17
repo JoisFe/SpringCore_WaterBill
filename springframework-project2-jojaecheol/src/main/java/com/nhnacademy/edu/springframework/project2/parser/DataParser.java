0b38fcf065c9a0603131d34a7242597b47b82dc9
조재철 <rhsnqk@gmail.com>
2022-04-17 21:15:46 +0900

    error delete

--- springframework-project2-jojaecheol/src/main/java/com/nhnacademy/edu/springframework/project2/parser/DataParser.java
+++
@@ -1,9 +0,0 @@
-package com.nhnacademy.edu.springframework.project2.parser;
-
-import com.nhnacademy.edu.springframework.project2.repository.Tariff;
-import java.util.List;
-import org.springframework.stereotype.Service;
-
-public interface DataParser {
-    List<Tariff> parse(String fileLocation);
-}
